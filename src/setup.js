a8da5b0d0a3e5f18391b8a1c77c1e33589250ea3
LeoDias-0 <leodiasleodias00@gmail.com>
2021-11-11 15:12:47 -0300

    Basic setup.

--- src/setup.js
+++ src/setup.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 
 const { NODE_ENV } = process.env
 
-const path = (NODE_ENV === 'prod') ? '.env' : 
+const path = (NODE_ENV === 'prod') ? '.env' : '.env.test'
 
 dotenv.config({
     path
