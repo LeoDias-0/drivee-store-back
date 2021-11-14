ba6b98aeaa70061c02016142165cf560a542b89e
LeoDias-0 <leodiasleodias00@gmail.com>
2021-11-14 20:49:58 -0300

    Bug fixes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import cors from 'cors'
 import express from 'express'
 import getItems from './controllers/getItems.js'
-import getItem from './controllers/getItem.js'
+// import getItem from './controllers/getItem.js'
 import signUp from './controllers/signUp.js'
 import signIn from './controllers/signIn.js'
 
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/items', getItems)
-app.get('/item/:id', getItem)
+// app.get('/item/:id', getItem)
 app.post('/sign-up', signUp)
 app.post('/sign-in', signIn)
 
--- src/controllers/getItems.js
+++ src/controllers/getItems.js
@@ -2,11 +2,11 @@ import connection from '../database/database.js'
 
 export default async function getItems(req, res){
     try{
-        const result = await connection.query(`SELECT * FROM items;`);
-        res.status(200).send(result.rows);
+        const result = await connection.query('SELECT * FROM items;')
+        res.status(200).send(result.rows)
     }
     catch(err){
-        console.log(err);
-        res.sendStatus(500);
+        console.log(err)
+        res.sendStatus(500)
     }
 }
